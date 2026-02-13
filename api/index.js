19168f70bcd586325739e748f453a156d2d2955c
jonahfidel <jonahfidel@gmail.com>
2026-02-13 18:37:04 -0500

    Attempt to fix Vercel routing.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import { app, ensureSchema } from '../server/app.js'
+import { parse } from 'node:url'
 
 export default async function handler(req, res) {
+  // Restore original path when Vercel rewrites /api/* to /api?__path=...
+  const parsed = req.url && parse(req.url, true)
+  if (parsed) {
+    const { __path, ...query } = parsed.query || {}
+    if (typeof __path === 'string' && __path) {
+      const qs = Object.keys(query).length
+        ? '?' + new URLSearchParams(query).toString()
+        : ''
+      req.url = '/api/' + __path + qs
+    }
+  }
   await ensureSchema()
   return app(req, res)
 }
